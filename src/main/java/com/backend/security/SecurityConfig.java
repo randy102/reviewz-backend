b948cf8e2c4ef187d61d4d9d46771846a817296d
Randy Liu <welldey102@gmail.com>
2020-03-30 11:12:53 +0700

    Disable CORS

--- src/main/java/com/backend/security/SecurityConfig.java
+++ src/main/java/com/backend/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable() //Turn off CSRF
+            .cors().and().csrf().disable() //Turn off CSRF nad CORS
             //.authorizeRequests()
             //.anyRequest().authenticated()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
