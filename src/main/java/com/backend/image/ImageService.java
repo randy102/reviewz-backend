178bf0ef385da5f17708538a2d371dc191a0b772
Randy Liu <welldey102@gmail.com>
2020-04-04 14:00:39 +0700

    Fix Service

--- src/main/java/com/backend/image/ImageService.java
+++ src/main/java/com/backend/image/ImageService.java
@@ -4,12 +4,14 @@ import org.bson.BsonBinarySubType;
 import org.bson.types.Binary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.util.Optional;
 
+@Service
 public class ImageService {
     @Autowired
     private ImageRepository imageRepository;
