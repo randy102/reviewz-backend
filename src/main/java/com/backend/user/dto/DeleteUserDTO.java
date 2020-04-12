c2af82fc152daba2b1d993f08387401bddea78fc
Randy Liu <welldey102@gmail.com>
2020-04-12 09:36:47 +0700

    Add: DeleteUserDTO

--- src/main/java/com/backend/user/dto/DeleteUserDTO.java
+++ src/main/java/com/backend/user/dto/DeleteUserDTO.java
@@ -3,12 +3,12 @@ package com.backend.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import java.util.Set;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class RegisterDTO {
-    private String username;
-    private String password;
+public class DeleteUserDTO {
+    private Set<String> ids;
 }
