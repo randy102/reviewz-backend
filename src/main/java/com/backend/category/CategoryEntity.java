e1f3ea276bff6481218cc63dedac5e096a599800
Minh Chien <sam@google.com>
2020-04-22 12:03:30 +0700

    category

--- src/main/java/com/backend/category/CategoryEntity.java
+++ src/main/java/com/backend/category/CategoryEntity.java
@@ -1,5 +1,28 @@
 package com.backend.category;
 
-public class CategoryEntity {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
+@Setter
+@ToString
+@Document(collection = "mr_category")
+@NoArgsConstructor
+public class CategoryEntity {
+    @Id
+    private String id;
+    private String name;
+
+    public CategoryEntity(String name){
+        this.name = name;
+    }
+
+    public CategoryEntity( String id, String name){
+        this.id = id;
+        this.name = name;
+    }
 }
